fe8f6c180fa621382d3ea37206ce375ad4b48c79
Maxim V <m4k51m15k4m@gmail.com>
2024-02-17 13:46:42 +0200

    add user app

--- sky_air/urls.py
+++ sky_air/urls.py
@@ -18,7 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
-
+    path("admin/",
+         admin.site.urls, ),
+    path("api/user/",
+         include("user.urls",
+                 namespace="user"), ),
+    path("__debug__/",
+         include("debug_toolbar.urls"), ),
 ]
