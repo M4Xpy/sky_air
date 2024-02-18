8a13906b57d1ea1a9bb2e93729359a65cb97fb10
Maxim V <m4k51m15k4m@gmail.com>
2024-02-18 19:39:22 +0200

    add filters in views.py

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import permissions
 from rest_framework import viewsets, mixins
 from rest_framework.pagination import PageNumberPagination
@@ -30,44 +32,209 @@ from airport.serializers import (
 )
 
 
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="country",
+            type=str,
+            description="Filter by country (case-insensitive)",
+            required=False,
+            location="query",
+        ),
+    ]
+)
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
     permission_classes = IsAdminOrIfAuthenticatedReadOnly,
 
+    def get_queryset(self):
+        country = self.request.query_params.get("country")
+        queryset = self.queryset
 
+        if country:
+            queryset = queryset.filter(
+                name__icontains=country, )
+
+        return queryset.distinct()
+
+
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="country",
+            type=str,
+            description="Filter by country (case-insensitive)",
+            required=False,
+            location="query",
+        ),
+    ]
+)
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.select_related("country")
     serializer_class = CitySerializer
     permission_classes = IsAdminOrIfAuthenticatedReadOnly,
 
+    def get_queryset(self):
+        country = self.request.query_params.get("country")
+        queryset = self.queryset
 
+        if country:
+            queryset = queryset.filter(
+                country__name__icontains=country, )
+
+        return queryset.distinct()
+
+
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="city",
+            type=str,
+            description="Filter by city (case-insensitive)",
+            required=False,
+            location="query",
+        ),
+    ]
+)
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.select_related("city")
     serializer_class = AirportSerializer
 
+    def get_queryset(self):
+        city = self.request.query_params.get("city")
+        queryset = self.queryset
 
-class RouteViewSet(viewsets.ModelViewSet):
+        if city:
+            queryset = queryset.filter(
+                city__name__icontains=city, )
+
+        return queryset.distinct()
+
+
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="source",
+            type=str,
+            description="Filter by source (case-insensitive)",
+            required=False,
+            location="query",
+        ),
+        OpenApiParameter(
+            name="destinations",
+            type=str,
+            description="Filter by destination IDs (comma-separated)",
+            required=False,
+            location="query",
+        ),
+    ]
+)
+class RouteViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Route.objects.select_related("source", "destination", )
     serializer_class = RouteSerializer
 
+    def get_queryset(self):
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
 
+        queryset = self.queryset
+
+        if source:
+            queryset = queryset.filter(
+                source__city__name__icontains=source, )
+        if destination:
+            queryset = queryset.filter(
+                destination__city__name__icontains=destination, )
+
+        return queryset.distinct()
+
+
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="type",
+            type=str,
+            description="Filter by type (case-insensitive)",
+            required=False,
+            location="query",
+        ),
+    ]
+)
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
     permission_classes = IsAdminOrIfAuthenticatedReadOnly,
 
+    def get_queryset(self):
+        type = self.request.query_params.get("type")
+        queryset = self.queryset
 
+        if type:
+            queryset = queryset.filter(
+                name__icontains=type, )
+
+        return queryset.distinct()
+
+
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="name",
+            type=str,
+            description="Filter by name (case-insensitive)",
+            required=False,
+            location="query",
+        ),
+    ]
+)
 class AirplaneViewSet(viewsets.ModelViewSet):
     queryset = Airplane.objects.all()
     serializer_class = AirplaneSerializer
     permission_classes = IsAdminOrIfAuthenticatedReadOnly,
 
+    def get_queryset(self):
+        name = self.request.query_params.get("name")
+        queryset = self.queryset
 
+        if name:
+            queryset = queryset.filter(
+                name__icontains=name, )
+
+        return queryset.distinct()
+
+
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="full_name",
+            type=str,
+            description="Filter by full name (case-insensitive)",
+            required=False,
+            location="query",
+        ),
+    ]
+)
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
     permission_classes = permissions.IsAdminUser,
+
+    def get_queryset(self):
+        full_name = self.request.query_params.get("full_name")
+        queryset = self.queryset
+
+        if full_name:
+            queryset = queryset.filter(
+                Q(first_name__icontains=full_name) | Q(last_name__icontains=full_name)
+            )
+
+        return queryset.distinct()
 
 
 class FlightViewSet(viewsets.ModelViewSet):
@@ -81,6 +248,17 @@ class OrderPagination(PageNumberPagination):
     max_page_size = 100
 
 
+@extend_schema(
+    parameters=[
+        OpenApiParameter(
+            name="country",
+            type=str,
+            description="Filter by country (case-insensitive)",
+            required=False,
+            location="query",
+        ),
+    ]
+)
 class OrderViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
